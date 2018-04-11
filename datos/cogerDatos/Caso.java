737defd594bc8ac0d11e8094483105db8bee9eef
alu0100813272 <alu0100813272@cc1313.etsii.ull.es>
2018-04-11 11:31:39 +0100

    Inicial

---
+++ datos/cogerDatos/Caso.java
@@ -0,0 +1,12 @@
+package cogerDatos;
+/**
+ * @author Jairo González Lemus. Email : alu0100813272@ull.edu.es Universidad:
+ *         Universidad de La Laguna. Práctica Asignatura: Modelado Sistemas
+ *         Sofware.
+ * @version 1.0
+ * @since 2018-03-14
+ */
+public class Caso {
+	
+
+}
